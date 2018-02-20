90261ab7efad5e7231843c7b76df5559e7d682f3
David Morse <webdevdjm@gmail.com>
2018-02-20 12:03:27 -0500

    Set up project

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# udacity-pixel-art-maker
+# Udacity Pixel Art Maker Project
+
+## Built for Udacity Grow with Google Challenge
+
+[Udacity Classroom](https://classroom.udacity.com/me)
+[Github Repo](https://github.com/udacity/project-pixel-art-maker-starter)
+
+I added some automation to the build process with [Browser Sync](https://www.npmjs.com/package/browser-sync)
+
+To use as a starter simply:
+1. Clone or Download this repo 
+2. Navigate to the folder in the terminal or command line
+3. Run npm init
+4. To start the browswer-sync server up, type in npm start. Note if you want to use a different browser the package.json file will need to be modified. I point it to the Firefox Developer Edition, but this can certainly be changed.
---
+++ designs.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+  'use strict';
+
+  // Variables
+  var colorChoice = $('#colorPicker');
+  var heightChoice = $('#inputHeight');
+  var widthChoice = $('#inputWidth');
+  var canvas = $('#pixelCanvas');
+  var isDrawing = false;
+
+  // Function to Make Grid
+  function makeGrid(){
+    canvas.find('tbody').remove();
+    var rows = heightChoice.val();
+    var columns = widthChoice.val();
+    canvas.append('<tbody></tbody>')
+    var canvasBody = canvas.find('tbody');
+    
+    for(var i = 0; i < rows; i++) {
+      canvasBody.append('<tr></tr>');
+      for(var j = 0; j < columns; j++){
+        $('tr:last').append('<td></td>');
+        $('td').attr('class', 'cells');
+      }
+    }
+  }
+  // End of grid maker
+
+  // Draw away
+  function draw(){
+    var isDrawing = false;
+    // NOTE to self: needs refactoring - var color = $('#colorPicker').val(); used 2x
+    $('#pixelCanvas').on('mousedown', 'td', function(evt) {
+      evt.preventDefault();
+      var color = $('#colorPicker').val();
+      isDrawing = true;
+      if (isDrawing) {
+        $(evt.target).css('background-color', color);
+      };
+    });
+    $('#pixelCanvas').on('mouseenter', 'td', function(evt) {
+      if (isDrawing) {
+        var color = $('#colorPicker').val();
+        $(evt.target).css('background-color', color);
+      }
+    });
+    $('#pixelCanvas').on('mouseup', 'td', function(evt) {
+      isDrawing = false;
+    });
+    $('#pixelCanvas').on('mouseleave', function(evt) {
+      isDrawing = false;
+    });
+  }
+
+  // Click to make the grid
+  $('input[type="submit"]').on('click', function(evt) {
+    evt.preventDefault();
+    // So once button is pushed we prevent the default behavior and make the grid!
+    makeGrid();
+  });
+
+  draw();
+
+});
